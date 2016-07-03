cb572fbc6b6db60539994426a89ddcc38a92e4ee
vadym.vasylenko <vadimtalne@gmail.com>
2016-07-03 19:59:34 +0300

    Users, Tags and main menu were added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  resources :tags
+  resources :users
   resources :posts
+  get "home" => "home#first"
+  root :to => "home#first"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
